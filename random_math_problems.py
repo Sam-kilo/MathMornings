87d823c1a437ed5ca1f86e1c1bcc054570ef0be8
Samuel Webb <adrenahurdle53@gmail.com>
2025-01-11 03:45:04 -0500

    I made some slight improvements. first inside random_math_problems.py I made it randomly select multiplication, subtraction, and addition. I also added some simple try and exept error to alarm clock.py

--- random_math_problems.py
+++ random_math_problems.py
@@ -3,17 +3,34 @@ import random
 
 correct_answer = 0
 max_problems = 3  # Number of problems to solve
+operator = ["+","-","*"]
 
-def add_problem():
+def generate_problem():
+    """ Generate a problem with random operator and"""
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+    random_operator = random.choice(operator)
+    result = None
+
+    # Perform the operation based on the operator
+    if random_operator == "+":
+        result = num1 + num2
+    elif random_operator == "-":
+        result = num1 - num2
+    elif random_operator == "*":
+        result = num1 * num2
+
+
+    return num1, num2, random_operator, result
+def math_problem():
     """ this program makes you answer a set of random addition problems """
     global correct_answer  # has to be global or correct_answer will reset every time
     finished_problem = False  # tracks if all problems are solved
 
     while correct_answer < max_problems:
-        num1 = random.randint(1, 10)
-        num2 = random.randint(1, 10)
-        result = num1 + num2
-        user_answer = int(input(f"What is the result of {num1} + {num2}?: "))
+        num1, num2, random_operator, result = generate_problem()
+
+        user_answer = int(input(f"What is the result of {num1} {random_operator} {num2}?: "))
 
         if user_answer == result:
             print("Good job!")
@@ -26,5 +43,6 @@ def add_problem():
     finished_problem = True
     return finished_problem  # Return whether all problems have been solved
 
-#add_problem() #uncomment to test
+
+#math_problem() #uncomment to test
 
